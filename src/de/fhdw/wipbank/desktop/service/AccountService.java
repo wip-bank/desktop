d4822e4eb14c8818160898f5ce2b541fe07528cc
dsawenko <daniel.sawenko@web.de>
2017-09-12 19:32:39 +0200

    Konto ohne Transaktionen können nun angezeigt werden

--- src/de/fhdw/wipbank/desktop/service/AccountService.java
+++ src/de/fhdw/wipbank/desktop/service/AccountService.java
@@ -1,6 +1,5 @@
 package de.fhdw.wipbank.desktop.service;
 
-
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -9,39 +8,43 @@ import de.fhdw.wipbank.desktop.model.Transaction;
 
 public class AccountService {
 
-    private static Account account;
+	private static Account account;
 
-    /**
-     * Gibt den Account des Benutzers zurück.
-     * 
-     * @return Account des Benutzers
-     */
-    public static Account getAccount() {
-        return account;
-    }
+	/**
+	 * Gibt den Account des Benutzers zurück.
+	 * 
+	 * @return Account des Benutzers
+	 */
+	public static Account getAccount() {
+		return account;
+	}
 
-    /**
-     * Setzt den Account des Benutzers auf den übergebenen Account.
-     *
-     * Es werden zusätzlich alle Transaktionen nach TransactionDate sortiert: die neusten Transaktionen sind dann zuerst in der Liste.
-     * Quelle: https://stackoverflow.com/a/5927408
-     *
-     * @param pAccount zu setzender Account
-     */
-    public static void setAccount(Account pAccount) {
+	/**
+	 * Setzt den Account des Benutzers auf den übergebenen Account.
+	 *
+	 * Es werden zusätzlich alle Transaktionen nach TransactionDate sortiert: die
+	 * neusten Transaktionen sind dann zuerst in der Liste. Quelle:
+	 * https://stackoverflow.com/a/5927408
+	 *
+	 * @param pAccount
+	 *            zu setzender Account
+	 */
+	public static void setAccount(Account pAccount) {
 
-        // Transaktionen sortieren: neuste Transaktionen zuerst
-        Collections.sort(pAccount.getTransactions(), new Comparator<Transaction>() {
+		if (pAccount.getTransactions() != null) {
+			// Transaktionen sortieren: neuste Transaktionen zuerst
+			Collections.sort(pAccount.getTransactions(), new Comparator<Transaction>() {
 
-            public int compare(Transaction t1, Transaction t2) {
-                if (t2.getTransactionDate() == null || t1.getTransactionDate() == null)
-                    return 0;
-                return t2.getTransactionDate().compareTo(t1.getTransactionDate());
-            }
-        });
+				public int compare(Transaction t1, Transaction t2) {
+					if (t2.getTransactionDate() == null || t1.getTransactionDate() == null)
+						return 0;
+					return t2.getTransactionDate().compareTo(t1.getTransactionDate());
+				}
+			});
+		}
 
-        // Account setzen
-        account = pAccount;
-    }
+		// Account setzen
+		account = pAccount;
+	}
 
 }
